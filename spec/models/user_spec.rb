e90989ea4e8e324e3be49c5b83b609c771df830e
Sahil Batsal <sahil.batsal@antiersolutions.com>
2023-03-03 13:42:37 +0530

    First commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a firstname,lastname and email" do
+    user = User.new 
+    (
+      email: 'madhvimishra338@gmail.com',
+      password: '123456'
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = User.new
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  
+  it "returns a user email as a string" do
+    user = User.new(email: 'khushi123@gmail.com')
+    expect(user)
+  end
+
+  it "is invalid with duplicate email address" do
+    User.create
+    (
+      email: 'arnavmadhvi12@gmail.com'
+    )
+    user = User.new
+    (
+      email: 'khushi123@gmail.com'
+    )
+    user.valid?
+    expect(user.errors[:email])
+  end
+
+  it "should have many albums" do
+    t = User.reflect_on_association(:albums)
+    expect(t.macro).to eq(:has_many)
+  end
 end
